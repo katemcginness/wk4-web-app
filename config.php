4e6cca2e3440cfd7107042803118c2970bf9ab63
kateemcg <88216309+katemcginness@users.noreply.github.com>
2021-08-16 16:18:56 +1000

    week 3 updates
    week 3

--- config.php
+++ config.php
@@ -9,4 +9,11 @@ $options = array(
     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
 
 );
+
+/* Attempt to connect to MySQL database */
+try{
+    $pdo_connection = new PDO($dsn, $username, $password, $options);
+} catch(PDOException $e){
+    die("ERROR: Could not connect. " . $e->getMessage());
+}
 ?>
--- public/templates/header.php
+++ public/templates/header.php
@@ -7,17 +7,20 @@
     <meta charset="utf-8">
 
     <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
 </head>
 
 <body>
     <header>
     <h1><a href="index.php">Your Artwork Collection tracker</a></h1>
 <nav>
-    <ul>
-        <li><a href="index.php">Home</a></li>
-        <li><a href="#">item 2</a></li>
-        <li><a href="#">item 3</a></li>
-        <li><a href="#">item 4</a></li>
+<ul>
+        <li><a href="create.php">Add a new artwork</a></li>
+        <li><a href="read.php">Find an artwork</a></li>
+        <li><a href="update.php">Update an artwork</a></li>
+        <li><a href="delete.php">Delete an artwork</a></li>
+        <li><a href="logout.php">Log out</a></li>
+        <li><a href="reset.php">Reset password</a></li>
     </ul>
 </nav>    
 </header>
